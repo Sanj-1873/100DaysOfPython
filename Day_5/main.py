13657860fe1b0d4a7b49c5dbb4b4f0f92e48cc4b
Sanj-1873 <sanjnac@gmail.com>
2022-01-29 20:49:34 +0530

    turtle code

---
+++ Day_5/main.py
@@ -0,0 +1,11 @@
+import turtle 
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkSeaGreen1")
+timmy.forward(100)
+
+
+my_screen = turtle.Screen()
+my_screen.exitonclick()
