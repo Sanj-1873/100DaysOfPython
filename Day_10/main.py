bad3fdc3f02d92c9a78a2e7caa19e9d1abe8bf5f
Sanj-1873 <sanjnac@gmail.com>
2022-02-04 22:10:45 +0530

    added snake game

--- Day_10/main.py
+++ Day_10/main.py
@@ -1,31 +1,27 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-snake = []
-starting_point = [0,-20,-40]
-for snake_pos in range(3):
-    body = Turtle(shape="square")
-    body.color("white")
-    body.penup()
-    body.goto(x=starting_point[snake_pos], y=0)
-    snake.append(body)
-    screen.update()
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(snake)-1, 0, -1):
-        new_x = snake[seg_num - 1].xcor()
-        new_y = snake[seg_num - 1].ycor()
-        snake[seg_num].goto(new_x, new_y)
-    
-    snake[0].forward(20)
+    snake.move()
+
 
 screen.exitonclick()
---
+++ Day_10/snake.py
@@ -0,0 +1,41 @@
+from turtle import Screen, Turtle
+
+STARTING_POSTIONS = [0, -20, -40]
+MOVE_DISTANCE = 20
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head  = self.segments[0]
+        
+    def create_snake(self):
+        for postion in range(3):
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(x=STARTING_POSTIONS[postion], y=0)
+            self.segments.append(new_segment)
+           
+    
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+
+    def down(self):
+        self.head.setheading(270)
+  
+
+    def left(self):
+        self.head.setheading(180)
+
+    
+    def right(self):
+        self.head.setheading(0)
+    
